138136170efefc710824f8bcaa03e1eedd96ad2a
Kevvol <helsinkiwhite@gmail.com>
2023-08-27 17:11:52 -0400

    added adding multiple particle

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 var ce;
 
 class Particle {
-    constructor(x0, y0, z0) {
+    constructor(x0, y0, z0, head_c, trail_c) {
         this.init_pos = [x0, y0, z0];
         this.pos_list = [this.init_pos,];
+
+        this.head_color = head_c;
+        this.trail_color = trail_c;
     }
 
     progress_tick(dt, exps, reps) {
@@ -66,9 +69,28 @@ function ijs_setup() {
     //access the expressions contained inside by equation_fields[i].expression
     equation_fields = document.getElementById("mid-2").getElementsByClassName('equation-field');
 
+    document.getElementById("add-item").addEventListener("click", add_item_clicked);
+
+    //create the first particle entry
+    add_item_clicked();
+
     document.getElementById("start-render").addEventListener("click", start_rendering_clicked);
     document.getElementById("pause-render").addEventListener("click", pause_rendering_clicked);
     document.getElementById("reset-sim").addEventListener("click", reset_simulation_clicked);
+}
+
+//add an initial particle to the simulation space
+function add_item_clicked() {
+    var clon = document.getElementById("templates").getElementsByClassName("particle-init-entry")[0].cloneNode(true);
+
+    clon.getElementsByClassName("remove-x")[0].addEventListener("click", function(e){
+        var entry_div = e.target.parentNode;
+        var parent_div = entry_div.parentNode
+
+        parent_div.removeChild(entry_div)
+    });
+
+    document.getElementById("particles-init-pos").appendChild(clon);
 }
 
 function get_init_pos() {
@@ -83,7 +105,10 @@ function get_init_pos() {
         var y0 = parseFloat(coords[1].value);
         var z0 = parseFloat(coords[2].value);
 
-        var p = new Particle(x0, y0, z0);
+        var particle_color = init_pos_fields[i].getElementsByClassName("particle-color")[0].value;
+        var trail_color = init_pos_fields[i].getElementsByClassName("trail-color")[0].value;
+
+        var p = new Particle(x0, y0, z0, particle_color, trail_color);
         res.push(p);
     }
 
@@ -93,7 +118,7 @@ function get_init_pos() {
 //when the start button is pressed
 function start_rendering_clicked() {
     reset_simulation_clicked();
-    
+
     //read the current equations into the equations list for usage
     current_equations = [];
     for (var i=0; i<equation_fields.length; i++) {
@@ -112,13 +137,16 @@ function start_rendering_clicked() {
     var scene = document.getElementById("vr-scene");
     //var templates = document.getElementById("vr-templates");
     for (var i=0; i<current_particles.length; i++) {
+        //var trail_color = current_particles[i].trail_color;
         var cur_line = document.createElement('a-entity');
         cur_line.setAttribute("class", "trajectory-lines");
+        //cur_line.setAttribute("material", "color:" + trail_color + ";");
 
+        var head_color = current_particles[i].head_color;
         var cur_head = document.createElement('a-entity');
         cur_head.setAttribute("class", "part-heads");
         cur_head.setAttribute("geometry", "primitive: cone; radiusBottom: 0.06; radiusTop: 0.0; height:0.12; segmentsRadial:3; ");
-        cur_head.setAttribute("material", "color:#00ff00;");
+        cur_head.setAttribute("material", "color:" + head_color + ";");
         cur_head.setAttribute("position", "0 0 0");
 
         scene.appendChild(cur_line);
@@ -205,7 +233,8 @@ function render_interval_func() {
             //"line__2", "line__3", etc. for each segment
             var attr_name = "line__" + current_particles[i].pos_list.length;
             //value will be like "start: 0 4 -3; end: 1 5 -3; color: white;"
-            var attr_valu = "start: " + prv_pos.join(" ") + "; end: " + cur_pos.join(" ") + "; color: white;";
+            var attr_valu = "start: " + prv_pos.join(" ") + "; end: " + cur_pos.join(" ") + "; color: " + current_particles[i].trail_color + ";";
+            //var attr_valu = "start: " + prv_pos.join(" ") + "; end: " + cur_pos.join(" ") + ";";
             //console.log(attr_valu);
             particle_trails[i].setAttribute(attr_name, attr_valu);
         }
