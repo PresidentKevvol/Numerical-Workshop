ecbac7d57e0a7e6db712d7ace5df500cc5976fc0
Kevvol <helsinkiwhite@gmail.com>
2024-04-09 14:10:07 -0400

    added touch up/down on mobile

--- solar-eclipse-sim/index.js
+++ solar-eclipse-sim/index.js
@@ -302,3 +302,16 @@ function ijs_setup() {
 }
 
 document.addEventListener("DOMContentLoaded", ijs_setup);
+
+//add up/down touch control on mobile
+AFRAME.components["look-controls"].Component.prototype.onTouchMove = function (t) {
+    var PI_2 = Math.PI/2,
+    e,
+    o = this.el.sceneEl.canvas,
+    i = this.yawObject,
+    j = this.pitchObject;
+    this.touchStarted && this.data.touchEnabled && (e = 2 * Math.PI * (t.touches[0].pageX - this.touchStart.x) / o.clientWidth, f = 2 * Math.PI * (t.touches[0].pageY - this.touchStart.y) / o.clientHeight, j.rotation.x += .3 * f, i.rotation.y += .5 * e, j.rotation.x = Math.max(-PI_2, Math.min(PI_2, j.rotation.x)), this.touchStart = {
+            x: t.touches[0].pageX,
+            y: t.touches[0].pageY
+        })
+    }
