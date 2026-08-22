3c1786320ef65c15e0bb7be7f022bf8f65a54b25
DeepFriedPancake <deepfriedpancakes4096@gmail.com>
2026-08-22 17:03:22 -0400

    rotation test

--- general-statics/superhands-grab-rotate-adhoc.js
+++ general-statics/superhands-grab-rotate-adhoc.js
@@ -4,40 +4,40 @@ since rotation is broken on super-hands 3.0.6
 */
 AFRAME.registerComponent('super-hands-rotate-adhoc', {
     init: function () {
-      const el = this.el;
+        const el = this.el;
 
-    this.gabber_hand = null;
-    this.being_grabbed = false;
-  
-      // Triggered when the controller begins grabbing the object
-      el.addEventListener('grab-start', function (evt) {
-        // el.setAttribute('material', 'posColor', {x: 1.0, y: 0.1, z: 0.1});
-        // document.getElementById("support-dodecahedron").setAttribute('color', '#ffbfbf');
-        
-        var hand = evt.detail.hand;
-        this.gabber_hand = hand;
-        this.being_grabbed = true;
-    });
-  
-      // Triggered when the controller releases the object
-      el.addEventListener('grab-end', function (evt) {
-        // el.setAttribute('material', 'posColor', {x: 0.9, y: 0.5, z: 0.1});
-        // document.getElementById("support-dodecahedron").setAttribute('color', '#bfffff');
+        this.el.gabber_hand = null;
+        this.el.being_grabbed = false;
+    
+        // Triggered when the controller begins grabbing the object
+        el.addEventListener('grab-start', function (evt) {
+            // el.setAttribute('material', 'posColor', {x: 1.0, y: 0.1, z: 0.1});
+            // document.getElementById("support-dodecahedron").setAttribute('color', '#ffbfbf');
+            
+            var hand = evt.detail.hand;
+            this.el.gabber_hand = hand;
+            this.el.being_grabbed = true;
+        });
+    
+        // Triggered when the controller releases the object
+        el.addEventListener('grab-end', function (evt) {
+            // el.setAttribute('material', 'posColor', {x: 0.9, y: 0.5, z: 0.1});
+            // document.getElementById("support-dodecahedron").setAttribute('color', '#bfffff');
 
-        // var hand = evt.detail.hand;
-        this.gabber_hand = null;
-        this.being_grabbed = false;
-    });
+            // var hand = evt.detail.hand;
+            this.el.gabber_hand = null;
+            this.el.being_grabbed = false;
+        });
     },
 
     tick: function () {
         // if it it being grabbed
-        if (this.being_grabbed) {
+        if (this.el.being_grabbed) {
             // log rotation on screen
             // const rotDeg = this.gabber_hand.getAttribute('rotation');
 
             var debug_div = document.getElementById("html-panel").getElementsByClassName("debug")[0];
-            debug_div.innerHTML = JSON.stringify(this.gabber_hand);
+            debug_div.innerHTML = JSON.stringify(this.el.gabber_hand);
             debug_div.innerHTML += `Rotation - X: ${rotDeg.x}, Y: ${rotDeg.y}, Z: ${rotDeg.z}`;
 
         }
