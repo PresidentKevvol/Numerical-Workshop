f52e25981ef6fd395248ef9be54fb377cd7c5c14
DeepFriedPancake <deepfriedpancakes4096@gmail.com>
2026-08-26 23:13:46 -0400

    fixed removing wrong code

--- chaos-attractor-vr/sim-update-component.js
+++ chaos-attractor-vr/sim-update-component.js
@@ -9,5 +9,16 @@ AFRAME.registerComponent('sim-update', {
     },
 
     tick: function (time, timeDelta) {
+      if (animation_playing) {
+        //update every n frame
+        if (this.tick_ct === 0) {
+            render_interval_func();
+        }
+
+        this.tick_ct += 1;
+        if (this.tick_ct % 2 === 0) {
+            this.tick_ct = 0;
+        }
     }
-  });
+    }
+});
